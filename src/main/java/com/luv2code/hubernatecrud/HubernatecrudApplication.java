faa71d7164123e2fe6e2fe05225891c1363a95c5
gauravstr07 <gauravstr05@gmail.com>
2026-05-14 23:16:51 +0530

    first commit

---
+++ src/main/java/com/luv2code/hubernatecrud/HubernatecrudApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.hubernatecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubernatecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubernatecrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luv2code/hubernatecrud/HubernatecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.hubernatecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubernatecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
